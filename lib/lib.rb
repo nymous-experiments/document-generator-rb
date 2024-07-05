2f01ee66c13ebe17c52fb5e5fd6bcca785279bc2
D0gma_ <d0gma@d0gma.fr>
2024-07-05 21:58:48 +0200

    Clean HexaPDF usage

--- lib/lib.rb
+++ lib/lib.rb
@@ -3,97 +3,59 @@
 require 'json'
 
 module DocumentGenerator
-  module Facture
+  module Invoice
     INPUT = JSON.parse('{
-      "id_facture": "001",
-      "date_vente": "2024-04-27",
-      "date_emission": "2024-04-27",
-      "nom_client": "Client Test",
-      "adresse_client": "123 Rue de Test\n59650 Ville Test",
+      "invoice_id": "001",
+      "sale_date": "2024-04-27",
+      "issue_date": "2024-04-27",
+      "client_name": "Client Test",
+      "client_address": "123 Test Street\n59650 Test City",
       "items": [
         {
-          "nom_item": "Article 1",
-          "prix": 10.00,
-          "quantite": 2
+          "item_name": "Item 1",
+          "price_cents": 1000,
+          "quantity": 2
         },
         {
-          "nom_item": "Article 2",
-          "prix": 20.00,
-          "quantite": 1
+          "item_name": "Item 2",
+          "price_cents": 2000,
+          "quantity": 1
         }
       ],
-      "date_paiement": "2024-04-27",
-      "moyen_paiement": "CB",
-      "montant_paiement": 40
+      "payment_date": "2024-04-27",
+      "payment_method": "Carte Bancaire",
+      "payment_amount_cents": 40
     }', symbolize_names: true)
 
-    FOOTER = <<~EOS
-      Le délai de paiement est de 45 jours + fin du mois, à partir de la date de facturation.
+    FOOTER = <<~FOOTER
+      Le délai de paiement est de 45 jours + fin du mois, à partir de la date de facturation (date d'émission de la facture).
       En cas de retard de paiement, seront exigibles, conformément à l'article L 441-6 du code de commerce, une indemnité calculée sur la base de trois fois le taux de l'intérêt légal en vigueur ainsi qu'une indemnité forfaitaire pour frais de recouvrement de 40 euros.
-    EOS
+    FOOTER
 
-    CLIENT_INFO = <<~EOS
-      Client Test
-      123 Rue de Test
-      59650 Ville Test
-    EOS
-
-    CONDITIONS = <<~EOS
-      Conditions de règlement : Prix comptant sans escompte : Chèque (Ordre : Rézoléo), Espèces ou Virement
+    CONDITIONS = <<~CONDITIONS
+      Conditions de règlement : Prix comptant sans escompte
+      Moyen de paiement : Chèque (ordre : Rézoléo), Espèces ou Virement
       Conditions de vente : Prix de départ
 
       (1) TVA non applicable, article 293 B du CGI
-    EOS
+    CONDITIONS
 
-    INFO_REZOLEO = <<~EOS
+    INFO_REZOLEO = <<~INFOS
       École Centrale de Lille - Avenue Paul Langevin - 59650 Villeneuve d'Ascq
       rezoleo@rezoleo.fr
       SIRET : 831 134 804 00010
       IBAN : FR76 1670 6050 8753 9414 0728 132
-    EOS
+    INFOS
 
-    class FactureMetadata
-      attr_reader :title
-      attr_reader :author
-      attr_reader :subject
-      attr_reader :creation_date
-
-      def initialize(facture_id:)
-        @title = 'Facture Rézoleo'
-        @author = 'Association Rézoléo'
-        @subject = "Facture #{facture_id}"
-        @creation_date = Time.now
-      end
-
-      def to_h
-        {
-          Title: @title,
-          Author: @author,
-          Subject: @subject,
-          CreationDate: @creation_date,
-        }
-      end
-    end
-
-    FactureMetadata2 = Data.define(:Title, :Author, :Subject, :CreationDate) do
-      def initialize(facture_id:)
+    PDFMetadata = Data.define(:title, :author, :subject, :creation_date) do
+      def initialize(invoice_id:)
         super(
-          Title: 'Facture Rézoléo',
-          Author: 'Association Rézoléo',
-          Subject: "Facture #{facture_id}",
-          CreationDate: Time.now,
+          title: "Facture Rézoléo #{invoice_id}",
+          author: 'Association Rézoléo',
+          subject: "Facture #{invoice_id}",
+          creation_date: Time.now
         )
       end
     end
-
-    def self.metadata(facture_id:)
-      {
-        Title: 'Facture Rézoléo',
-        Author: 'Association Rézoléo',
-        Subject: "Facture #{facture_id}",
-        CreationDate: Time.now
-      }
-    end
   end
-
 end
--- main_hexa.rb
+++ main_hexa.rb
@@ -6,32 +6,44 @@ require_relative 'lib/lib'
 
 BASE_FONT_SIZE = 12
 
-class FactureComposer < HexaPDF::Composer
-  def initialize(skip_page_creation: false, page_size: :A4, page_orientation: :portrait,
-                 margin: 36)
+# The `InvoiceComposer` class is responsible for creating invoices in PDF format
+# using the HexaPDF library. It sets up the document configuration, including
+# fonts and styles, and provides methods to add pages with a predefined layout.
+# This class is a subclass of `HexaPDF::Composer` and extends its functionality
+# to meet the specific requirements of Rezoleo's invoice creation.
+class InvoiceComposer < HexaPDF::Composer
+  def initialize(skip_page_creation: false, page_size: :A4, page_orientation: :portrait, margin: 36)
     super(skip_page_creation:, page_size:, page_orientation:, margin:)
 
+    # The `pdfa` task ensures that the generated PDF is PDF/A compliant.
     # https://hexapdf.gettalong.org/documentation/pdfa/index.html
     document.task(:pdfa)
 
+    config_font(font_name: 'DejaVu Sans',
+                font_file: 'assets/DejaVuSans.ttf',
+                bold_font_file: 'assets/DejaVuSans-Bold.ttf')
+
+    config_style(page_size:)
+  end
+
+  def config_font(font_name:, font_file:, bold_font_file:)
     document.config['font.map'] = {
-      'DejaVu Sans' => {
-        none: 'assets/DejaVuSans.ttf',
-        bold: 'assets/DejaVuSans-Bold.ttf',
+      font_name.to_s => {
+        none: font_file.to_s,
+        bold: bold_font_file.to_s
       }
     }
+  end
+
+  def config_style(page_size:)
     style(:base, font: 'DejaVu Sans', font_size: BASE_FONT_SIZE, line_spacing: 1.2)
     style(:bold, font: ['DejaVu Sans', { variant: :bold }])
-    style(:header, font: ['DejaVu Sans', { variant: :bold }], font_size: BASE_FONT_SIZE * 7 / 6, text_align: :center) # TODO: Use 14 as hardcoded?
-    style(:footer, font_size: 7, text_align: :center) # TODO: Use `BASE_FONT_SIZE / 2`?
+    style(:header, font: ['DejaVu Sans', { variant: :bold }], font_size: BASE_FONT_SIZE * 7 / 6, text_align: :center)
+    style(:footer, font_size: BASE_FONT_SIZE / 2, text_align: :center)
+    style(:table_header, font: 'DejaVu Sans', font_size: BASE_FONT_SIZE * 0.75)
+    style(:conditions, font_size: BASE_FONT_SIZE * 5 / 6, fill_color: '3C3C3C')
 
-    page_style(:default, page_size: page_size) do |canvas, style|
-      # canvas.font('DejaVu Sans', size: 12)
-      # canvas.text("Coucou", at: [50, 50], position: :flow)
-      margins = HexaPDF::Layout::Style::Quad.new(0)
-      # margins.top =
-      # style.frame = style.create_frame(canvas.context, margins)
-    end
+    page_style(:default, page_size:)
   end
 
   def new_page(style = @next_page_style)
@@ -40,7 +52,7 @@ class FactureComposer < HexaPDF::Composer
     image('assets/logo_rezo.png', width: 75, position: :float, mask_mode: :none)
     text('Facture Rézoléo', style: :header, margin: margin_bottom(2))
 
-    text(DocumentGenerator::Facture::FOOTER, style: :footer, position: [0, 0], text_align: :center)
+    text(DocumentGenerator::Invoice::FOOTER, style: :footer, position: [0, 0])
   end
 end
 
@@ -48,10 +60,15 @@ def margin_bottom(lines)
   [0, 0, lines * BASE_FONT_SIZE]
 end
 
-input = DocumentGenerator::Facture::INPUT
-doc_metadata = DocumentGenerator::Facture::FactureMetadata.new(facture_id: input[:id_facture])
+def format_cents(cents)
+  # float conversion to avoid integer division causing rounding errors
+  "#{format('%.2f', cents.to_f / 100)}€"
+end
 
-composer = FactureComposer.new(skip_page_creation: true)
+input = DocumentGenerator::Invoice::INPUT
+doc_metadata = DocumentGenerator::Invoice::PDFMetadata.new(invoice_id: input[:invoice_id])
+
+composer = InvoiceComposer.new(skip_page_creation: true)
 
 # composer.document.config['debug'] = true
 
@@ -62,99 +79,95 @@ composer.document.metadata.creation_date(doc_metadata.creation_date)
 
 composer.new_page
 
-facture_header = <<~EOS
-Facture n°001
-Date de vente : 2024-04-27
-Date d'émission : 2024-04-27
-EOS
-composer.text(facture_header, text_align: :center, margin: margin_bottom(2))
+invoice_header = <<~HEADER
+  Facture n°#{input[:invoice_id]}
+  Date de vente : #{input[:sale_date]}
+  Date d'émission : #{input[:issue_date]}
+HEADER
+
+composer.text(invoice_header, style: :base, text_align: :center, margin: margin_bottom(2))
 
 composer.text('Association Rézoléo (Trésorerie)', style: :bold, margin: margin_bottom(1))
 
-composer.text(DocumentGenerator::Facture::INFO_REZOLEO, margin: margin_bottom(2))
+composer.text(DocumentGenerator::Invoice::INFO_REZOLEO, margin: margin_bottom(2))
 
 composer.text('Client', style: :bold, margin: margin_bottom(1))
 
-composer.text(DocumentGenerator::Facture::CLIENT_INFO, margin: margin_bottom(3))
+composer.text(input[:client_name], margin: margin_bottom(1))
+composer.text(input[:client_address], margin: margin_bottom(3))
 
-items = [
-  { nom_item: 'Article 1', prix: 1000, quantite: 2 },
-  { nom_item: 'Article 2', prix: 2000, quantite: 1 },
-]
-
-header = lambda do |tb|
+header = lambda do |_tb|
   [
     { background_color: 'C0C0C0' },
     [
-      # TODO: Fix this once HexaPDF header supports passing only strings, like for data
-      composer.document.layout.text('ID'),
-      composer.document.layout.text('Désignation article'),
-      composer.document.layout.text('Prix unit. HT', text_align: :right),
-      composer.document.layout.text('Quantité', text_align: :right),
-      composer.document.layout.text('TVA (1)', text_align: :right),
-      composer.document.layout.text('Total TTC', text_align: :right),
+      composer.document.layout.text('ID', style: :table_header),
+      composer.document.layout.text('Désignation article', style: :table_header),
+      composer.document.layout.text('Prix unit. HT', style: :table_header, text_align: :right),
+      composer.document.layout.text('Quantité', style: :table_header, text_align: :right),
+      composer.document.layout.text('TVA (1)', style: :table_header, text_align: :right),
+      composer.document.layout.text('Total TTC', style: :table_header, text_align: :right),
     ]
   ]
 end
+
 data = []
+items = input[:items]
 
 items.each_with_index do |item, i|
   item_id = i + 1
-  item_name = item[:nom_item]
-  price_in_euros = item[:prix] / 100
-  quantity = item[:quantite]
+  item_name = item[:item_name]
+  price_in_cents = item[:price_cents]
+  quantity = item[:quantity]
 
   data << [
     item_id.to_s,
     item_name,
-    composer.document.layout.text("#{'%.2f' % price_in_euros}€", text_align: :right),
+    composer.document.layout.text(format_cents(price_in_cents), text_align: :right),
     composer.document.layout.text(quantity.to_s, text_align: :right),
     composer.document.layout.text('0%', text_align: :right),
-    composer.document.layout.text("#{'%.2f' % (price_in_euros * quantity)}€", text_align: :right),
+    composer.document.layout.text(format_cents(price_in_cents * quantity), text_align: :right)
   ]
 end
 
-total_price = items.map { |it| it[:prix] * it[:quantite] }.sum / 100
+total_price_in_cents = items.map { |it| it[:price_cents] * it[:quantity] }.sum
+
 data << [
   { content: 'Total', col_span: 5 },
-  composer.document.layout.text("#{'%.2f' % total_price}€", text_align: :right),
+  composer.document.layout.text(format_cents(total_price_in_cents), text_align: :right)
 ]
 
-composer.table(data, column_widths: [-2, -8, -5, -4, -4, -4], header: header, margin: margin_bottom(2))
+composer.table(data, column_widths: [-1, -9, -3, -2, -2, -3], header:, margin: margin_bottom(2))
 
-composer.text("Somme totale hors taxes (en euros, HT) : #{'%.2f' % total_price}€")
-composer.text("Somme totale à payer toutes taxes comprises (en euros, TTC) : #{'%.2f' % total_price}€", margin: margin_bottom(1))
+composer.text("Somme totale hors taxes (en euros, HT) : #{format_cents(total_price_in_cents)}")
+composer.text("Somme totale à payer toutes taxes comprises (en euros, TTC) : #{format_cents(total_price_in_cents)}",
+              margin: margin_bottom(1))
 
-composer.text DocumentGenerator::Facture::CONDITIONS, font_size: 10, fill_color: '3C3C3C', margin: margin_bottom(3)
+composer.text(DocumentGenerator::Invoice::CONDITIONS, style: :conditions, margin: margin_bottom(3))
 
-header = lambda do |tb|
+header = lambda do |_tb|
   [
-    { background_color: 'C0C0C0', font_size: 10 },
+    { background_color: 'C0C0C0' },
     [
-      # TODO: Fix this once HexaPDF header supports passing only strings, like for data
-      composer.document.layout.text('Date'),
-      composer.document.layout.text('Règlement'),
-      composer.document.layout.text('Montant', text_align: :right),
-      composer.document.layout.text('À payer', text_align: :right),
+      composer.document.layout.text('Date', style: :table_header),
+      composer.document.layout.text('Règlement', style: :table_header),
+      composer.document.layout.text('Montant', style: :table_header, text_align: :right),
+      composer.document.layout.text('À payer', style: :table_header, text_align: :right)
     ]
   ]
 end
 
-data = [
-  { font_size: 10 },
-  [
-    '2024-04-27',
-    'CB',
-    composer.document.layout.text('40.00€', text_align: :right),
-    composer.document.layout.text('0.00€', text_align: :right)
-  ]
-]
+payed_in_cents = input[:payment_amount_cents] || 0
+left_to_pay_in_cents = total_price_in_cents - payed_in_cents
 
-composer.table(data, header: header, width: 400)
+payment_method = input[:payment_method] || ''
 
-# composer.document.pages.each do |page|
-#   box = page.canvas.context.box(:media)
-#   page.canvas.font('DejaVu Sans', size: 12)
-# end
+data = [[
+  composer.document.layout.text(input[:payment_date], style: :table_header),
+  composer.document.layout.text(payment_method, style: :table_header),
+  composer.document.layout.text(format_cents(payed_in_cents), style: :table_header, text_align: :right),
+  composer.document.layout.text(format_cents(left_to_pay_in_cents), style: :table_header, text_align: :right)
+]]
+
+composer.table(data, column_widths: [-3, -5, -2, -2], header:, width: 300)
 
 composer.write('demo_hexa.pdf')
